c979e3d4873651effd00e8c12bb9a25ca189853a
JulesDrouet <jules.drouet44@gmail.com>
2023-11-29 11:09:19 +0100

    ajout chronometre marioooooo

---
+++ Assets/Assets/Scripts/Chronometer.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Chronometer : MonoBehaviour
+{
+    public TMP_Text timeText;
+
+    public PlayerHealth playerHealth;
+
+    public float time;
+
+
+
+    void Update()
+    {
+        if(playerHealth.currentHealth > 0) 
+        {
+        time += Time.deltaTime;
+
+        var minutes = Mathf.Floor(time / 60); //Divide the guiTime by sixty to get the minutes.
+        var seconds = time % 60;//Use the euclidean division for the seconds.
+        var fraction = (time * 100) % 100;
+
+        //update the label value
+        timeText.text = string.Format("{0:00} : {1:00} : {2:00}", minutes, seconds, fraction);
+        }
+        else
+        {
+
+        }
+    }
+}
