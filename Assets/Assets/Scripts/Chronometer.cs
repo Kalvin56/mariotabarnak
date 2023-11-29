eb2eb5f42f69d550a39a9504ec95376899f650ac
JulesDrouet <jules.drouet44@gmail.com>
2023-11-29 11:16:10 +0100

    indentation

--- Assets/Assets/Scripts/Chronometer.cs
+++ Assets/Assets/Scripts/Chronometer.cs
@@ -16,16 +16,16 @@ public class Chronometer : MonoBehaviour
 
     void Update()
     {
-        if(playerHealth.currentHealth > 0) 
+        if (playerHealth.currentHealth > 0)
         {
-        time += Time.deltaTime;
+            time += Time.deltaTime;
 
-        var minutes = Mathf.Floor(time / 60); //Divide the guiTime by sixty to get the minutes.
-        var seconds = time % 60;//Use the euclidean division for the seconds.
-        var fraction = (time * 100) % 100;
+            var minutes = Mathf.Floor(time / 60); //Divide the guiTime by sixty to get the minutes.
+            var seconds = time % 60;//Use the euclidean division for the seconds.
+            var fraction = (time * 100) % 100;
 
-        //update the label value
-        timeText.text = string.Format("{0:00} : {1:00} : {2:00}", minutes, seconds, fraction);
+            //update the label value
+            timeText.text = string.Format("{0:00} : {1:00} : {2:00}", minutes, seconds, fraction);
         }
     }
 }
